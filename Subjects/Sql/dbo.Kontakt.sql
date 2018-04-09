cca501a7ff2831d87ffd38cee89c9ca5aa4bf09a
jura <jirapanek@gmail.com>
2018-04-09 21:01:18 +0200

    Database added
    Subjekt and Kontakt tables
    
    Signed-off-by: jura <jirapanek@gmail.com>

---
+++ Subjects/Sql/dbo.Kontakt.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Kontakt] (
+    [Id]            INT           IDENTITY (1, 1) NOT NULL,
+    [Jmeno]         NVARCHAR (60) NOT NULL,
+    [Prijmeni]      NVARCHAR (60) NOT NULL,
+    [DatumNarozeni] DATE          NOT NULL,
+    [Telefon]       NVARCHAR (16) NULL,
+    [Vlozeno]       DATETIME      NULL,
+    [Ico]           INT           NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Kontakt_Subjekt] FOREIGN KEY ([Ico]) REFERENCES [Subjekt] ([Ico])
+);
+
---
+++ Subjects/Sql/dbo.Subjekt.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Subjekt]
+(
+	[Ico] INT NOT NULL PRIMARY KEY, 
+    [Nazev] NVARCHAR(60) NULL, 
+    [Ulice] NVARCHAR(60) NULL, 
+    [Obec] NVARCHAR(60) NULL, 
+    [Psc] NCHAR(5) NULL, 
+    [Poznamka] NVARCHAR(MAX) NULL, 
+    [Vlozeno] DATETIME NULL
+)
